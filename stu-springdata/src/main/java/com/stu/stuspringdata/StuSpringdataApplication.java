bd2acbcd127e6641a3f43ea08df77042fb5a9e7a
satells <msergiost@hotmail.com>
2021-11-05 15:32:42 -0300

    start

---
+++ stu-springdata/src/main/java/com/stu/stuspringdata/StuSpringdataApplication.java
@@ -0,0 +1,13 @@
+package com.stu.stuspringdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StuSpringdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StuSpringdataApplication.class, args);
+	}
+
+}
---
+++ stu-springdata/src/test/java/com/stu/stuspringdata/StuSpringdataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stu.stuspringdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StuSpringdataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
